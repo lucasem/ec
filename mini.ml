c0e16202670c2b01ec88225a83732b09d519b26e
Lucas Morales <lucas@lucasem.com>
2017-03-21 01:58:52 -0400

    more interesting mini.ml

--- mini.ml
+++ mini.ml
@@ -3,9 +3,9 @@ open Interface
 let prims = C.prims @ Str_ops.combs
 
 let main () =
-  let s = "((B ((S (C (substr 0))) (findchar <DOT>))) ((S (C ((B ((C substr) (-1 0))) +1))) (findchar <LESS-THAN>)))" in
+  let s = "((B ((B ((replace-substr-all empty) (string-of-char <SPACE>))) (feach ((substr 0) (+1 0))))) (filter-words ((S is) cap)))" in
   let f = Expr.unmarshal s prims in
-  let x = Expr.of_str "#include <os.h>" in
+  let x = Expr.of_str "Structure and Interpretation of Computer Programs" in
   let q = Expr.Application(f, x) in
   let r = Expr.run q in
   print_string (match r with
